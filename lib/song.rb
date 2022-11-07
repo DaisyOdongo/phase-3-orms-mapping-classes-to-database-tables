6da7a8b048c145ffa765bf522e3e6d93da8ee07e
+ <+>
2022-11-07 17:34:34 +0300

    completed

--- lib/song.rb
+++ lib/song.rb
@@ -21,33 +21,31 @@ class Song
 
   def save
     sql = <<-SQL
-    INSERT INTO songs (nam, album)
+    INSERT INTO songs (name, album)
     VALUES (?, ?)
     SQL
-
-    #insert the song
-    DB[:conn].execute(sql, self.name, self.album)
+    DB[:conn].execute(sql, self.name, self.album)#insert the song
   
 
   #get the song ID from the database and save it to the ruby instatnce
-  self.id = DB[:conn].execute("SELECT last_insert rowid() FROM songs")[0][0]
-
+      self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   #return the Ruby instance
   self
   end
 
-   def self.create(name:, album:)
+  def self.create(name:, album:)
     song = Song.new(name: name, album: album)
     song.save
   end
 
 end
 
-# hello = Song.new(name: "Hello", album: "25")
-# hello.save
-# gold_digger = Song.new(name: "Gold Digger", album: "Late Registration")
-# gold_digger.save
 
- song = Song.create(name: "Hello", album: "25")
- song.name
- song.album
+ #hello = Song.new(name: "Hello", album: "25")
+ #hello.save
+#gold_digger = Song.new(name: "Gold Digger", album: "Late Registration")
+#gold_digger.save
+
+ #song = Song.create(name: "Hello", album: "25")
+ #song.name
+ #song.album
